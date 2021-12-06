e39a997817bed282553f154318f2a5b8677647ff
George <g.nikolaropoulos@gmail.com>
2021-12-06 07:28:35 +0200

    day 06 completed

--- day06/main.py
+++ day06/main.py
@@ -1,16 +1,44 @@
+from collections import Counter
+
+
 def solve_part_1(input):
-    pass
+    count = Counter(input)
+
+    for i in range(80):
+        new_count = Counter()
+
+        for fish, c in count.items():
+            if fish == 0:
+                new_count[6] += c
+                new_count[8] += c
+            else:
+                new_count[fish - 1] += c
+
+        count = new_count
+    return sum(count.values())
 
 
 def solve_part_2(input):
-    pass
+    count = Counter(input)
 
+    for i in range(256):
+        new_count = Counter()
+
+        for fish, c in count.items():
+            if fish == 0:
+                new_count[6] += c
+                new_count[8] += c
+            else:
+                new_count[fish - 1] += c
+
+        count = new_count
+    return sum(count.values())
 
 def get_puzzle_input():
     puzzle_input = []
     with open("input.txt") as input_txt:
         for line in input_txt:
-            puzzle_input.append(line.strip())
+            puzzle_input = [int(x) for x  in line.strip().split(",")]
     return puzzle_input
 
 
