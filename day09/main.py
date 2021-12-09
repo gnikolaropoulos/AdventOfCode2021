490f10a467ad75fee934d143e8042007228b8854
George <g.nikolaropoulos@gmail.com>
2021-12-09 08:06:56 +0200

    day 09 completed

--- day09/main.py
+++ day09/main.py
@@ -1,9 +1,78 @@
+from functools import reduce
+
+
 def solve_part_1(input):
-    pass
+    width = len(input[0])
+    height = len(input)
+    lows = []
+    for y in range(height):
+        for x in range(width):
+            current = int(input[y][x])
+            left = 10
+            if x-1 > -1:
+                left = int(input[y][x-1])
+            right = 10
+            if x+1 < width:
+                right = int(input[y][x+1])
+            up = 10
+            if y-1 > -1:
+                up = int(input[y-1][x])
+            down = 10
+            if y+1 < height:
+                down = int(input[y+1][x])
+            if current < left and current < right and current < up and current < down:
+                lows.append(current)
+    print(lows)
+    return sum(x+1 for x in lows)
 
 
 def solve_part_2(input):
-   pass
+    width = len(input[0])
+    height = len(input)
+
+    identified = set()
+    basins = []
+
+    for y in range(height):
+        for x in range(width):
+            val = int(input[y][x])
+            if val == 9 or (y, x) in identified:
+                continue
+            basin = {(y, x)}
+            frontier = [(y, x)]
+
+            for dy, dx in frontier:
+                for ny, nx in neighs_bounded(dy, dx, 0, height - 1, 0, width - 1):
+                    nval = int(input[ny][nx])
+                    if nval < 9 and (ny, nx) not in basin:
+                        basin.add((ny, nx))
+                        frontier.append((ny, nx))
+
+            basins.append(len(basin))
+            for by, bx in basin:
+                identified.add((by, bx))
+
+    basins.sort()
+    print(basins)
+    return basins[-1]*basins[-2]*basins[-3]
+
+
+def neighs_bounded(y, x, min_height, max_height, min_width, max_width):
+    neighs = []
+
+    if y > min_height:
+        neighs.append([y-1, x])
+
+    if y < max_height:
+        neighs.append([y+1, x])
+
+    if x > min_width:
+        neighs.append([y, x-1])
+
+    if x < max_width:
+        neighs.append([y, x+1])
+
+    return neighs
 
 
 def get_puzzle_input():
