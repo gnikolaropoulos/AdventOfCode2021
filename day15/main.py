560e566006553694832264e103e6fe2d8cc63765
George <g.nikolaropoulos@gmail.com>
2021-12-15 11:05:58 +0200

    day 15 completed

--- day15/main.py
+++ day15/main.py
@@ -1,16 +1,65 @@
+import sys
+from collections import defaultdict
+from heapq import heappush, heappop
+
+
 def solve_part_1(input):
-    pass
+    heap = [((0, 0), 0)]
+    location_costs = defaultdict(lambda: sys.maxsize)
+    while len(heap) > 0:
+        (i,j), current_cost = heappop(heap)
+        for neighbor in ((i, j + 1), (i + 1, j)):
+            if neighbor not in input:
+                continue
+
+            neighbor_cost = current_cost + input[neighbor]
+            if neighbor_cost < location_costs[neighbor]:
+                location_costs[neighbor] = neighbor_cost
+                heappush(heap,(neighbor, neighbor_cost))
+
+    return location_costs[max(location_costs.keys())]
 
 
 def solve_part_2(input):
-    pass
+    width, height = max(input.keys())
+    height += 1
+    width += 1
+
+    new_grid = input.copy()
+    for dj in range(5):
+        for di in range(5):
+            risk_increase = dj + di
+            if risk_increase == 0:
+                continue
+
+            for (i, j), cost in input.items():
+                new_cost = cost + risk_increase
+                if new_cost > 9:
+                    new_cost -= 9
+                new_grid[(i + (di * width), j + (dj * height))] = new_cost
+
+    heap = [((0, 0), 0)]
+    location_costs = defaultdict(lambda: sys.maxsize)
+    while len(heap) > 0:
+        (i,  j), current_cost = heappop(heap)
+        for neighbor in ((i, j+1), (i, j-1), (i+1, j), (i-1, j)):
+            if neighbor not in new_grid:
+                continue
+
+            neighbor_cost = current_cost + new_grid[neighbor]
+            if neighbor_cost < location_costs[neighbor]:
+                location_costs[neighbor] = neighbor_cost
+                heappush(heap, (neighbor, neighbor_cost))
+
+    return location_costs[max(location_costs.keys())]
 
 
 def get_puzzle_input():
-    puzzle_input = []
+    puzzle_input = {}
     with open("input.txt") as input_txt:
-        for line in input_txt:
-            puzzle_input.append(line.strip())
+        for j, line in enumerate(input_txt):
+            for i, c in enumerate(line.strip()):
+                puzzle_input[(i, j)] = int(c)
     return puzzle_input
 
 
